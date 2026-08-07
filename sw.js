47d4dddc1a5f55460b999225ff298c15ec3130f1
omarnageh84-commits <omarnageh84@gmail.com>
2026-08-07 08:07:30 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='mashrooie-v6';
-const ASSETS=['./','./index.html','./js/app.js','./js/sheets.js','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
