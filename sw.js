e804866340972de52d1ed7674fcb60f9c20de93c
omarnageh84-commits <omarnageh84@gmail.com>
2026-08-07 21:33:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,39 @@
-const CACHE="yawmeyati-v6";
-const FILES=["./","./index.html","./manifest.json","./css/style.css","./js/app.js","./js/daily.js","./js/attendance.js","./js/notes.js","./js/tasks.js","./js/dashboard.js","./js/sheets.js"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener("fetch",e=>{
+const CACHE_NAME = 'yawmeyati-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/style.css',
+  './js/app.js',
+  './js/dashboard.js',
+  './js/sheets.js',
+  './js/daily.js',
+  './js/attendance.js',
+  './js/tasks.js',
+  './js/notes.js',
+  './js/utils.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+});
+
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r=>{
-      return r || fetch(e.request).then(res=>{
-        caches.open(CACHE).then(c=>c.put(e.request,res.clone()));
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      return caches.open(CACHE_NAME).then(cache => {
+        cache.put(e.request, res.clone());
         return res;
-      }).catch(()=>r);
-    })
+      });
+    }).catch(()=> caches.match('./index.html')))
   );
 });
