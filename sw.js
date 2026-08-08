f0458a1b41a1b8048da18ccbfca95c58a84eee96
omarnageh84-commits <omarnageh84@gmail.com>
2026-08-08 14:30:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-
+const CACHE='yawmeyati-v7-ultra';
+const ASSETS=['./','./index.html','./css/style.css','./js/app.js','./js/utils.js','./js/sheets.js','./js/dashboard.js','./js/daily.js','./js/attendance.js','./js/notes.js','./js/tasks.js','./manifest.json'];
+self.addEventListener('install',e=>{self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))); self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
